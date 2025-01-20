4ccc42f0c85f6edbb07abc5a3949143eb374e11c
Jos Dehaes <jos.dehaes@gmail.com>
2025-01-20 11:54:53 +0100

    improve release script

--- release.sh
+++ release.sh
@@ -13,26 +13,41 @@ set -e
 # This also requires the github-cli tool to be installed and logged in
 
 KEY=12CE6799A94A3F1B5DDFFE88F576553597FB8FEB
-
 REPO=asahi-alarm
-
+TAG=aarch64
 RELEASE=false
 
 # Parse options
-while getopts ":crh" opt; do
+while getopts ":R:crp:t:h" opt; do
   case $opt in
+    R)
+      REPO=$OPTARG
+      if [ ! -d "$REPO" ]; then
+        mkdir -p "../$REPO"
+      fi
+      ;;
     c)
       echo "cleaning files"
-      rm -f ../$REPO/*.xz ../$REPO/*.sig ../$REPO/asahi-alarm.db* ../$REPO/asahi-alarm.files* || true
+      rm -f ../$REPO/*.xz ../$REPO/*.sig ../$REPO/$REPO.db* ../$REPO/$REPO.files* || true
       ;;
     r)
       RELEASE=true
       ;;
+    t)
+      TAG=$OPTARG
+      ;;
     h)
-      echo "Usage: $0 [-r] [-c] [PKGS]"
-      echo "\t -r: create a release on github"
-      echo "\t -c: clean the packages in ../$REPO before building"
+      echo "Usage: $0 [-R <dir>] [-r] [-c] -p <gpg passphrase> -t <tag> [PKGS]"
+      echo -e "    -R <dir>: place packages in <dir>. Provide this first to override the default 'asahi-alarm'"
+      echo -e "    -p <passphrase>: pass in the passphrase as argument instead of asking for it"
+      echo -e "    -r: create a release on github. Default: false"
+      echo -e "    -t <tag>: use <tag> as tag on the release git repo. This will also be the release name. This only \
+        has effect if -r is passed. Default: aarch64"
+      echo -e "    -c: clean the packages in ../$REPO before building. Default: false"
       exit 1
+      ;;
+    p)
+      GPG_PASSPHRASE=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -46,29 +61,41 @@ shift $((OPTIND - 1))
 
 # default packages, can be overridden on command line
 # TODO: remove lsp-plugins once a new version is released
-PKGS="linux-asahi lsp-plugins alsa-ucm-conf-asahi bankstown speakersafetyd asahi-audio calamares asahi-calamares-configs asahi-configs asahi-fwextract asahi-alarm-keyring asahi-scripts lzfse m1n1 mesa-asahi tiny-dfr uboot-asahi xkeyboard-config-asahi widevine asahi-desktop-meta asahi-meta"
+# NOTE fex-emu-rootfs-arch needs the rootfs to be placed in the same folder as the PKGBUILD as
+# default.erofs.xz to work.
+PKGS="linux-asahi lsp-plugins alsa-ucm-conf-asahi bankstown speakersafetyd asahi-audio calamares \
+  asahi-calamares-configs asahi-configs asahi-fwextract asahi-alarm-keyring asahi-scripts lzfse \
+  m1n1 mesa-asahi tiny-dfr uboot-asahi xkeyboard-config-asahi widevine asahi-desktop-meta asahi-meta\
+  virglrenderer-asahi libkrun libkrunfw muvm FEX-Emu fex-emu-rootfs-arch vulkan-tools asahi-bless steam"
 
 if [ $# -ge 1 ]; then
   PKGS=("$@")
 fi
 
-echo "building packages: ${PKGS[*]}"
+if [ -z "$GPG_PASSPHRASE" ]; then
+  echo "Enter GPG passphrase:"
+  read -s GPG_PASSPHRASE
+fi
 
-echo "Enter GPG passphrase:"
-read -s GPG_PASSPHRASE
+echo "RELEASE=$RELEASE"
+echo "REPO=$REPO"
+echo "TAG=$TAG"
+echo "RELEASE=$RELEASE"
 
 # Build packages
 for srcpkg in $PKGS; do
   pushd $srcpkg
   echo "Building $srcpkg"
-  makepkg -Cfs --noconfirm
-  pkg=$(ls -- *.pkg.tar.xz)
-  # there could be multiple packages
+  # Remove any previous created packages
+  rm -f *.pkg.tar.xz *.pkg.tar.xz.sig
+  makepkg -Cs --noconfirm
+  # NOTE mesa-asahi also builds the fex overlays, but these need to be built on x86, so these
+  # packages should be ignored for release
+  pkg=$(ls -- *.pkg.tar.xz | grep -v fex-emu-overlay)
+  # there could be multiple packages built from same PKGBUILD
   for bin in $pkg; do
-    # Remove any previous created signatures
-    rm -f "${bin}".sig || true
     echo "$GPG_PASSPHRASE" | gpg --local-user $KEY --pinentry-mode loopback --passphrase-fd 0 --detach-sign "${bin}"
-    cp "$bin"* ../../$REPO/
+    mv "$bin"* ../../$REPO/
     repo-add --key $KEY --sign ../../$REPO/$REPO.db.tar.gz "../../$REPO/$bin"
   done
   popd
@@ -77,9 +104,9 @@ done
 # Release packages on github if requested
 if [ "$RELEASE" = true ]; then
   cd ../$REPO
-  git tag -d aarch64 && git tag aarch64 && git push -f --tags
+  git tag -d $TAG && git tag $TAG && git push -f --tags
 
-  yes | gh release delete aarch64
-  gh release create aarch64 --notes ""
-  gh release upload aarch64 -- *.xz *.xz.sig asahi-alarm.db* asahi-alarm.files*
+  gh release delete -y $TAG
+  gh release create $TAG --notes ""
+  gh release upload $TAG -- *.xz *.xz.sig $REPO.db* $REPO.files*
 fi
